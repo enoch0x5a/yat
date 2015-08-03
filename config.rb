d1d58a38c9b9fdbd2819bf62bdea6ddb72c2e029
enoch0x5a <enoch0x5a@gmail.com>
2015-08-03 15:28:43 +0300

    daily commit

--- config.rb
+++ config.rb
@@ -1,4 +1,5 @@
 Options = {
-  :api_key => 'trnsl.1.1.20150727T094607Z.fa989f7cb7c98817.f0abcceb21d32fa734cdcba406cfc78053f78484',
-  :path => 'https://translate.yandex.net/api/v1.5/tr.json',
+  :key => 'trnsl.1.1.20150727T094607Z.fa989f7cb7c98817.f0abcceb21d32fa734cdcba406cfc78053f78484',
+  :json_path => 'https://translate.yandex.net/api/v1.5/tr.json',
+  :xml_path => 'https://translate.yandex.net/api/v1.5/tr'
 }
